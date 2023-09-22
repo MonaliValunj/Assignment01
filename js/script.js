e28fcd04a0e0edcac219d57f0b5341d7b0d526fa
Monali Valunj <monavalunj96@gmail.com>
2023-09-21 22:06:21 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,108 @@
+//STEP 1
+
+/* let someMonth
+function theMonth() {}
+let currentMonth
+let summerMonth
+let MyLibraryfFunction
+ */
+
+//STEP 2
+  // numeric literal expressin
+  // let num = 2
+
+  //  string literal expression
+  //let str = "John Smith"
+
+  //Boolean literal expression
+  //let bln = true
+
+  //null literal expression
+  //let val1 = null
+
+// STEP 3
+/* 
+let x = 20
+let y = 3
+let z = 10
+let result = (x * y) / z
+*/
+
+//STEP 4
+/* 
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let numZipCode
+let numYourAge
+let strReferralSource
+let blnMayWeContactYou
+ */
+
+// STEP5
+/* 
+let firstName, city, zipCode
+firstName = "MONALI"
+city = "San Diego"
+zipCode = 123456
+ */
+
+//let firstName = "Monali", city = "San Diego", zipCode = 123456
+
+/* let firstName = "Monali"
+let city = "San Diego"
+let zipCode = 234567
+ */
+
+//STEP 6
+/*
+let val = 'My favorite number is: ' + 5
+console.log( val)
+let val2 = '5' + 5
+console.log(val2) 
+*/
+
+//STEP 7
+/*
+let result1 = true + " this is boolean"
+console.log(result1)
+
+let result2 = 56 + true
+console.log(result2) 
+*/
+
+//STEP 8
+/* 
+let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`
+console.log(someString)
+ */
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
+
+//STEP 18
+
+//STEP 19
+
+//STEP 20
+
+//STEP 21
+
+//STEP 22
