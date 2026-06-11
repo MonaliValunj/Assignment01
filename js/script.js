4d947d07d296f2fa86500749931ade572ebee0b7
Monali Valunj <monavalunj96@gmail.com>
2026-06-11 07:09:47 -0700

    6 steps completed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
 // STEP 1
 // let someMonth
-// function theMonth()
+// function theMonth() {}
 // let currentMonth
 // let summerMonth
 // let MyLibraryFunction
 
 // STEP 2
+// let num = 24
+// let str = "Monali Valunj"
+// let bln = true
+// let valNull = null
+
+// STEP 3
+// let num1 = 10
+// let num2 = 5
+// let total = 0
+
+// // Complex expression 1
+// total = num1 + num2 * 2
+
+// // Complex expression 2
+// total = (num1 - num2) / 5
 
 
+// STEP 4
+// let strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let strZipCode
+// let intYourAge
+// let strReferralSource
+// let boolMayWeContactYou
 
-// STEP 1
+
+// STEP 5
+// // Method 1: Declare and assign at the same time
+// let strFirstName = "Monali"
+
+// // Method 2: Declare first, then assign a value later
+// let intYourAge
+// intYourAge = 30
+
+// // Method 3: Declare multiple variables in one statement and assign values
+// let strCity = "Carlsbad", strState = "California"
 
 
+// STEP 6
+// let myVar = 10
 
-// STEP 1
+// console.log(myVar + " apples")
 
-
-
-// STEP 1
+// STEP 7
