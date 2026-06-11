b11bde047066136b66dbe575eddbc70648e67ec3
Monali Valunj <monavalunj96@gmail.com>
2026-06-11 06:50:30 -0700

    step 1 completed

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let MyLibraryFunction
+
+// STEP 2
+
+
+
+// STEP 1
+
+
+
+// STEP 1
+
+
+
+// STEP 1
